3ecbcc052a36f79834345702a559b712c8f592e6
Axel Hayalian <axforjob.hay@gmail.com>
2025-09-17 13:23:04 +0200

    Page About Me

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -3,9 +3,42 @@
 
 <template>
     <div id="about-me">
-        <h2 class="text-highlight-1 section-title"></h2>
+        <h2 class="text-highlight-1 section-title">Qui suis-je ?</h2>
+        <p class="default-text">
+            <span class="text-highlight-2">Développeur Web</span> depuis octobre 2023 et diplômé en juillet 2024, <span class="text-highlight-2">Full-Stack</span>. <br/>
+            J'ai commencé à travailler en tant qu'<span class="text-highlight-2">Opticien lunettier</span>, après cela, j'ai fait divers boulots,
+            pour enfin finir par trouver ma vocation qui m'a amené vers cette <span class="text-highlight-2">reconversion</span>. <br/><br/>
+
+            Je suis quelqu'un qui est <span class="text-highlight-2">curieux</span>, je n'ai pas peur d'<span class="text-highlight-2">apprendre</span> de nouvelles techno. 
+            J'aime la <span class="text-highlight-2">créativité</span> et l'<span class="text-highlight-2">innovation</span>, comprendre et répondre aux besoins d'un client. <br/>
+            L'<span class="text-highlight-2">IA</span> est aussi quelque chose qui m'intrigue, comment pouvons nous l'utiliser correctement. <br/><br/>
+            
+            
+            <u>(Attention !!!! à revoir l'affichage de la liste)</u>
+            <ul>Back-end :
+                <li>Mon Framework PHP sur lequel j'ai le plus travailler est Symfony</li>
+                <li>Pour le langage Java, le projet est de me familiariser avec un framework en plus de mes connaissances du langage</li> <br/>
+
+                Front-end :
+                <li>Utilisation de Vue.js</li> <br/>
+
+                Gestion de projet : 
+                <li>GitHub</li>
+            </ul>
+        </p>
     </div>
 </template>
 
 <style>
+    #about-me {
+        margin-top: 80px;
+    }
+
+    .default-text {
+        text-align: justify;
+    }
+
+    .default-text span {
+        font-weight: bold;
+    }
 </style>
